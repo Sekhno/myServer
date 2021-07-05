490b22c42c4326b98809a924baf9590e06063d7a
Dmitry Sekhno <dmytro.sekhno@wtt-solutions.com>
2021-07-05 21:12:54 +0300

    first commit

---
+++ public/javascripts/chat.js
@@ -0,0 +1,17 @@
+
+
+$(function () {
+    var socket = io();
+
+    $('form').submit(function(e) {
+        console.log('Submit')
+        e.preventDefault(); // prevents page reloading
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    socket.on('chat message', function(msg){
+        $('#messages').append($('<li>').text(msg));
+    });
+});
---
+++ socket.js
@@ -0,0 +1,14 @@
+var socketio = require('socket.io');
+
+function init(server) {
+    var io = socketio(server);
+    io.on('connection', function (socket) {
+        console.log("socket connected");
+        socket.on('newEvent', function (data) {
+            console.log(data);
+        });
+    });
+    return io;
+}
+
+module.exports = init;
