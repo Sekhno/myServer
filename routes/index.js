f693b5078ddf8a4bc10b47d16385d4414bdc6cf8
Dmytro Sekhno <bassugbor@gmail.com>
2021-10-30 16:22:40 +0300

    async await

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+function getList() {
+  return new Promise((resolve) => setTimeout(() => {
+    resolve(
+      [
+        {
+          id: 1,
+          title: 'Angular 12',
+          description: 'Изучить фреймворк!'
+        },
+        {
+          id: 2,
+          title: 'React',
+          description: 'Изучить фреймворк!'
+        },
+        {
+          id: 3,
+          title: 'React Native',
+          description: 'Изучить фреймворк!'
+        }
+      ]
+    )
+  }))
+}
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dmitry Sekhno' });
+router.get('/', async (req, res, next) => {
+  res.render('index', { title: 'Dmitry Sekhno', list: await getList() });
 });
 
 module.exports = router;
+
+
