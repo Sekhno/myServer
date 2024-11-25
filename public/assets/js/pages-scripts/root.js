c07de8a21c60db482301aada0e5a3edd6ba93beb
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2024-11-25 20:22:16 +0200

    Index page #3 - update cart for root page

--- public/assets/js/pages-scripts/root.js
+++ public/assets/js/pages-scripts/root.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //         $('#exampleModal').modal('show');
     //     }, 2500);
     // });
+    setTimeout(retrieveCartData);
+
     const jQuery = $;
     const $cartContainer = jQuery('#cart-container');
     const $countBadge = $cartContainer.find('.cart_qty_cls');
@@ -27,7 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
     jQuery(document).on('click',TARGET, function(e) {
         if (jQuery(this).data(KEY_TARGET) === '#addtocart') {
             const id = jQuery(this).data('product-id');
-            console.log("Натиснуто на кнопку з data-bs-target='#addtocart'", id);
 
             fetch('/api/v1/product/cart', {
                 method: 'POST',
@@ -35,16 +36,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({ id })
-            }).then((e) => {
-                const sum = $total.text() || 0;
-
-                $total.text(sum + 1);
-                $countBadge.show();
-            })
+            }).then(retrieveCartData);
         }
     })
 
-    setTimeout(retrieveCartData);
+
 
     function openSearch() {
         document.getElementById("search-overlay").style.display = "block";
@@ -77,6 +73,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }).then(({cart, products}) => {
             console.log('->', cart, products);
 
+            $cartContainer.find('.is-cloned').remove()
+
             if (cart && cart.length) {
                 let total = 0;
                 $countBadge.text(cart.length);
@@ -85,6 +83,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     const {name, price, images, currency} = products.find(({_id}) => productId === _id)
                     const $clone = $media.clone();
 
+                    console.log('$media',$media);
+                    console.log('$clone',$clone)
+
+                    $clone.addClass('is-cloned')
                     $clone.find('img').attr('src', images[0] + '?im=Resize,width=120');
                     $clone.find('.media-body > a > h4').text(name);
                     $clone.find('.media-body > h4 > span').text(`${quantity} x ${price} ${currency}`);
@@ -96,7 +98,13 @@ document.addEventListener('DOMContentLoaded', () => {
                         $clone.hide(100);
                         cart.splice(i, 1);
                         $countBadge.text(cart.length);
+
+                        if (!cart.length) {
+                            $countBadge.hide();
+                            $cartContainer.find('ul').hide();
+                        }
                     })
+                    $clone.show();
 
                     $media.after($clone);
                     total += quantity*price;
@@ -104,6 +112,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 $total.find('span').text(`${total}`);
                 $media.hide();
+                $countBadge.show();
+                $cartContainer.find('ul').show();
             }
             else {
                 $countBadge.hide();
